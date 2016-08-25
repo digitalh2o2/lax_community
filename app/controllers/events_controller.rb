0b8c3a9af2dde445fe7669e6e9ec1428d41c906f
Saul Ocampo <digitalh2o@gmail.com>
2016-08-24 22:58:30 -0500

    Working on Events Controller and features

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,37 @@
 class EventsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_current_event, only: [:show]
+
+	def index
+		@events = Event.all
+		authorize User
+	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.create(event_params)
+		if @event.save
+			redirect_to events_path
+		else
+			flash[:alert] = "Description too short!"
+			redirect_to new_event_path
+		end
+	end
+
+	def show
+
+	end
+
+	private
+
+	def set_current_event
+		@event = Event.find(params[:id])
+	end
+
+	def event_params
+		params.require(:event).permit(:name, :description)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcome#home'
-  
+
   resources :fields
   resources :comments
   resources :events
