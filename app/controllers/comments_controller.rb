ff69ca24ea59170dcc590d585f037c000b5c77b7
Saul Ocampo <digitalh2o@gmail.com>
2016-08-25 21:16:42 -0500

    Comments are working properly. Will clean up UI

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,47 @@
 class CommentsController < ApplicationController
+	before_action :set_current_comment, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@event = Event.find(params[:event_id])
+		@comments = Comment.all
+	end
+
+	def show
+
+	end
+
+	def new
+		@event = Event.find(params[:event_id])
+		@comment = Comment.new
+	end
+
+	def create
+		@event = Event.find(params[:event_id])
+		@comment = create_comment
+		redirect_to event_comments_path(@event)
+	end	
+
+	def update
+	end
+
+	def edit
+	end
+
+	def destroy
+
+	end
+
+	private
+
+	def set_current_comment
+		@comment = Comment.find(params[:id])
+	end
+
+	def create_comment
+		@event.comments.create(content: params[:comment][:content], user_id: current_user.id, event_id: @event.id)
+	end
+
+	def comment_params
+		params.require(:comment).permit(:content, :user_id, :event_id)
+	end 
 end
