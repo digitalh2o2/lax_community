ec3d98d2adbf4059432bec217a13586f4c95ed1f
Saul Ocampo <digitalh2o@gmail.com>
2016-08-26 20:26:23 -0500

    Adding Facebook Omniauth Integration without storing App and Secret publically

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   validates :username, presence: true, length: {minimum: 3}
   validates :username, uniqueness: true
@@ -14,5 +14,18 @@ class User < ActiveRecord::Base
   def full_name
   	self.first_name + " " + self.last_name
   end
+
+  def self.from_omniauth(auth)
+  	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  		user.provider = auth.provider
+  		user.uid = auth.uid
+  		user.email = auth.info.email
+  		user.password = Devise.friendly_token[0,20]
+  		user.first_name = auth.info.first_name
+  		user.last_name = auth.info.last_name
+  		user.username = auth.info.username
+  	end
+  end
+
 
 end
